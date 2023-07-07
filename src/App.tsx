c449bfc122cce604218caa1fb3b8b89ad9c01b34
Ivan Bykov <renbrek@gmail.com>
2023-07-07 20:04:00 +0300

    feature: add folder selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,63 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
+import { open } from '@tauri-apps/api/dialog';
+import './App.css';
+import { readDir, BaseDirectory, FileEntry } from '@tauri-apps/api/fs';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
+  const [selectedFolder, setSelectedFolder] = useState<string | null>(null);
+  const [files, setFiles] = useState<FileEntry[]>();
 
   async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }));
   }
+
+  const readFileContents = async () => {
+    try {
+      const selectedPath = await open({
+        multiple: false,
+        directory: true,
+        title: 'Open Text File',
+      });
+      setSelectedFolder(selectedPath as string);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedFolder)
+      readDir(selectedFolder, {
+        dir: BaseDirectory.AppData,
+        recursive: true,
+      }).then((files) => {
+        setFiles(files);
+        console.log(files);
+      });
+  }, [selectedFolder]);
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
       <p>{greetMsg}</p>
+      <button type="submit" onClick={readFileContents}>
+        Open File
+      </button>
+      <div>Selected Folder: {selectedFolder}</div>
+      {files ? (
+        <div>
+          Files:
+          {files.map((file) => (
+            <div key={file.path}>{file.name}</div>
+          ))}
+        </div>
+      ) : selectedFolder ? (
+        <>No files</>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
---
+++ src/components/FileItem.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react';
+
+interface FileItemProps {}
+
+const FileItem: FC<FileItemProps> = ({}) => {
+  return <div></div>;
+};
+export default FileItem;
